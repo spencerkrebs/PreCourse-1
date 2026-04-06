635a601f217e43b69449b21ea5e53979279f9a5b
spencerkrebs <krebsspencer@gmail.com>
2026-04-05 20:10:32 -0400

    Exercise 1 stack

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,34 @@
+# // Time Complexity :
+# // Space Complexity :
+# // Did this code successfully run on Leetcode :
+# // Any problem you faced while coding this :
+
+
+# // Your code here along with comments explaining your approach
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+      self.stack = []
          
      def isEmpty(self):
+      return len(self.stack) == 0
          
      def push(self, item):
+      self.stack.append(item)
          
      def pop(self):
-        
-        
+      return self.stack.pop(-1)
+          
      def peek(self):
+      return self.stack[-1]
         
      def size(self):
+      return len(self.stack)
          
      def show(self):
+      return self.stack
          
 
 s = myStack()
